22d2488964f56a5000622b7e16a18310474b1c1e
张涛 <zhangtao@oschina.cn>
2016-01-05 11:27:28 +0800

    加入gradle

---
+++ RxVolley/rxvolley/src/androidTest/java/com/kymjs/rxvolley/RxVolleyTest.java
@@ -0,0 +1,60 @@
+package com.kymjs.rxvolley;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author kymjs (http://www.kymjs.com/) on 1/5/16.
+ */
+public class RxVolleyTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRequestQueue() throws Exception {
+
+    }
+
+    @Test
+    public void testSetRequestQueue() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testGet1() throws Exception {
+
+    }
+
+    @Test
+    public void testPost() throws Exception {
+
+    }
+
+    @Test
+    public void testJsonGet() throws Exception {
+
+    }
+
+    @Test
+    public void testJsonPost() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCache() throws Exception {
+
+    }
+
+    @Test
+    public void testDownload() throws Exception {
+
+    }
+}
